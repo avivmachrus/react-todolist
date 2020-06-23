3a3644d4c880e8a16f579bf957e3bf8e65e564d5
avivmachrus <avivmachrus@gmail.com>
2020-06-23 12:13:20 +0700

    create header section

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import "./App.css";
 function App() {
   return (
     <div className="container">
-      <div className="frame"></div>
+      <div className="frame">
+        <div className="header">
+          <button className="header-btn">Add</button>
+          <h1 className="header-title">Todo Lists</h1>
+          <button className="header-btn main-red-color">Clear</button>
+        </div>
+      </div>
     </div>
   );
 }
