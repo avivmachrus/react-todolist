2073fba0e013ea989eb89f09fb14f023b73c7659
avivmachrus <avivmachrus@gmail.com>
2020-06-24 18:04:32 +0700

    create addTodo function in TodoList page

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -12,7 +12,7 @@ const TodoForm = ({ addTodo }) => {
       alert("Empty Form Not Allowed");
       return;
     }
-    alert(value);
+    addTodo(value);
     setValue("");
   };
 
--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -12,10 +12,18 @@ const TodoList = () => {
     { text: "Learning React!" },
   ]);
 
+  const addTodo = (value) => {
+    // take recent todos from todos(...todos) and add with new todos with value
+    const addedTodo = [...todos, { text: value }];
+
+    // set addedTodo to todos
+    setTodos(addedTodo);
+  };
+
   return (
     <Paper>
       <Header />
-      <TodoForm />
+      <TodoForm addTodo={addTodo} />
       <Todos todos={todos} />
     </Paper>
   );
