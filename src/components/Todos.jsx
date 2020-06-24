a5c43de1c8989df6acdf4995310a28458e7dc880
avivmachrus <avivmachrus@gmail.com>
2020-06-24 17:16:33 +0700

    Todos and Todolist refactor

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,13 +1,9 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 import Todo from "./Todo";
 
-const Todos = () => {
-  const todos = [
-    { text: "belajar react" },
-    { text: "belajar react" },
-    { text: "belajar react" },
-    { text: "belajar react" },
-  ];
+const Todos = ({ todos }) => {
   return (
     <section className="todos">
       {todos.map((todo) => {
@@ -15,6 +11,14 @@ const Todos = () => {
       })}
     </section>
   );
+};
+
+Todos.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string,
+    })
+  ),
 };
 
 export default Todos;
